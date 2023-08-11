2170e98f1e8ede41aec07ec7950e8828c2a91189
NourEldin77 <noureldean.2003@gmail.com>
2023-08-11 16:40:48 -0700

    0x01-variables_if_else_while/a.out

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
