63d76533fa76ac8c05bac8507a2b3b7610ffe297
NourEldin77 <noureldean.2003@gmail.com>
2023-08-16 16:50:02 -0700

    0x02-functions_nested_loops/11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_to_98 - Entry point
+ * @n: decimal num
  * Return: Always (0) Success
 */
-
 void print_to_98(int n)
-    {
-        while ( n < 98 ){
-            printf("%d, " , n);
-            n++;
-        }
-        while ( n > 98 )
-        {
-            printf("%d, ", n);
-            n--;
-        }
-    }
+{
+	while (n < 98)
+	{
+		printf("%i, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%i, ", n);
+		n--;
+	}
+	printf("98");
+	putchar('\n');
+}
