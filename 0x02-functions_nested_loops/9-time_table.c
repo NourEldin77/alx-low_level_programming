b4aff0c70b797b2a421ed88c6bc42114e0579d9d
NourEldin77 <noureldean.2003@gmail.com>
2023-08-16 18:23:16 -0700

    0x02-functions_nested_loops/9-time_table.c

---
+++ 0x02-functions_nested_loops/9-time_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void times_table(void)
+{
+	int i,y;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (y = 0; y <= ; y= y*9)
+		{
+			putchar(',');
+			putchar(' ');
+		}i
+		putchar('\n');
+	}
+
+}
