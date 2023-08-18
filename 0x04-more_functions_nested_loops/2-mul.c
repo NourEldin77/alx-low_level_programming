cfcb03cf92c1f535cf2bdf5eec8bd2d759da96ad
NourEldin77 <noureldean.2003@gmail.com>
2023-08-17 18:38:06 -0700

    0x04-more_functions_nested_loops/2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - mylti numbers
+ * @a: first num
+ * @b: second num
+ * Return: (0)
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - print decimal number with 10 number
+ * Return: (0)
+ */
+void print_numbers(void)
+{
+int n
+for (n = 48; n < 58; n++)
+{
+_putchar(n);
+}
+_putchar(10);
+}
