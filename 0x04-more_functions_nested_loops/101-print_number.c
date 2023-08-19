830a2a0f16e53caa484635a00fc24b8daf9ee786
NourEldin77 <noureldean.2003@gmail.com>
2023-08-18 19:41:08 -0700

    0x04-more_functions_nested_loops/101-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - prints num
+ *  @n: int num
+ */
+void print_number(int n)
+{
+	unsigned int j = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		j = -j;
+	}
+	if (j / 10)
+	{
+		print_number(j / 10);
+	}
+	_putchar(j % 10 + '0');
+}
