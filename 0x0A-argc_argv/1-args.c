9dc7cd5cca6d1b717f303f5dcc31785e0a65a6d1
NourEldin77 <noureldean.2003@gmail.com>
2023-09-01 14:21:23 -0700

    0x0A-argc_argv/1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints the exe file name
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always (0) Success
+ */
+int main(int argc, char *argv[])
+{
+	_putchar(argc);
+	_putchar('\n');
+	return (0);
+}
