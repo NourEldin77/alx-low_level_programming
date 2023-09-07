19ce2b4d8ac94f809aca08494c482d5709acd8f7
NourEldin77 <noureldean.2003@gmail.com>
2023-09-07 04:43:47 +0300

    score

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * argstostr - dosomthing
+ * @ac: any
+ * @av: any
+ * Return: char
+ */
+char *argstostr(int ac, char **av)
+{
+	(void)ac;
+	(void)av;
+	return (NULL);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * strtow - do something
+ * @str: any
+ * Return: char
+ */
+char **strtow(char *str)
+{
+	(void)str;
+	return (NULL);
+}
