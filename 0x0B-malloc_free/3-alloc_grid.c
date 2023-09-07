9660de05cff96b9b6c75677bc1abafe18fb10f98
NourEldin77 <noureldean.2003@gmail.com>
2023-09-07 04:35:21 +0300

    alloc_grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * alloc_grid - do something
+ * @width: any
+ * @height: any
+ * Return: int
+ */
+
+int **alloc_grid(int width, int height)
+{
+	(void)width;
+	(void)height;
+	return (0);
+}
