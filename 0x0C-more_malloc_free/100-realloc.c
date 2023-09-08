518ac6942e31ae76aa039d9dd311c8d69310c206
NourEldin77 <noureldean.2003@gmail.com>
2023-09-08 05:47:59 +0300

    calloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _realloc - dosomething
+ * @ptr: any
+ * @old_size: any
+ * @new_size:any
+ * Return: nothing void
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	(void)ptr;
+	(void)old_size;
+	(void)new_size;
+}
