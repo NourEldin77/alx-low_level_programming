bfba7298d77d822f8ee055c9695ffd31040cdfa4
NourEldin77 <noureldean.2003@gmail.com>
2023-09-09 04:13:55 +0300

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,7 +8,37 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	(void)ptr;
-	(void)old_size;
-	(void)new_size;
+	char *p;
+	unsigned int i;
+	/** Hard code for edge cases*/
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (!p)
+		{
+			return (NULL);
+		}
+		return (p);
+	}
+	/** main */
+	p = malloc(new_size);
+	if (!p)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		p[i] = ((char *) ptr)[i];
+	}
+	free(ptr);
+	return (p);
 }
