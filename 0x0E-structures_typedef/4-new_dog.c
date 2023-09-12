5bc14dc9b48fe2a9929ccbb1819a69a78de448e0
NourEldin77 <noureldean.2003@gmail.com>
2023-09-12 05:20:28 +0300

    score 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+/**
+ * new_dog - create new struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	void(name);
+	void(age);
+	void(owner);
+	return ((dog_t *)(uintptr_t)NULL);
+}
