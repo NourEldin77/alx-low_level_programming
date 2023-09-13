ef9045d9b6d03f06d32185267ae23e8872dd2198
NourEldin77 <noureldean.2003@gmail.com>
2023-09-13 19:51:19 +0300

    int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * int_index - do_something
+ * @array: any
+ * @size: any
+ * @cmp: any
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (array != NULL && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(*(array + i)))
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
