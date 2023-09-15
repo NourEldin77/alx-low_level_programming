a8a778aa922350dddb7e238905672d0f0338a982
NourEldin77 <noureldean.2003@gmail.com>
2023-09-14 19:38:58 -0700

    print

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,29 +1,45 @@
-#include "variadic_functtions.h"
+#include "variadic_functions.h"
 /**
  * print_all - print argument
  * @format: c or i or f or s
  * Return: void
  */
+
 void print_all(const char * const format, ...)
 {
 	va_list pa;
 	int i = 0;
+	char *c;
 
 	va_start(pa, format);
 	while (format != NULL &&  format[i] != '\0')
 	{
 		switch (format[i])
 		{
+			if (i > 0 && (format[i] == 'c' || format[i] == 'i' ||
+			      format[i] == 'f' || format[i] == 's'))
+			{
+			printf(", ");
+			}
 			case 'c':
-				printf("%s", va_arg(pa, char));
+				printf("&c%c", (char)va_arg(pa, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(pa, int));
+				printf("&i%d", va_arg(pa, int));
+				break;
 			case 'f':
-				prinf("%f", va_arg(pa, float));
-			case 's'
-				printf("%s", va_arg(pa, char*));
-				
+				printf("&f%f", va_arg(pa, double));
+				break;
+			case 's':
+				c =  va_arg(pa, char *);
+				printf("%s", c !=  (char *)NULL ? c : "(nil)");
+				break;
+			default:
+				i++;
+				continue;
 		}
+		i++;
 	}
+	va_end(pa);
+	printf("\n");
 }
