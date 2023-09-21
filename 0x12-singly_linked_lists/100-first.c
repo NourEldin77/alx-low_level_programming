f1d256f2dc6902391317a683eff21f2f5c2e7a80
NourEldin77 <noureldean.2003@gmail.com>
2023-09-22 02:30:19 +0300

    exe_first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * before_main - exe before the main func
+ * Return:void
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n%s",
+			"I bore my house upon my back!\n");
+}
