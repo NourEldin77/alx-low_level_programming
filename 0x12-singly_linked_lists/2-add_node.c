1a9e2f9e95f926c5fd6a4470932c23b7e4c28cab
NourEldin77 <noureldean.2003@gmail.com>
2024-03-03 02:27:23 +0200

    Task: add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,45 @@
 #include "lists.h"
+
+
 /**
- * add_node - do something
- * @head: param
- * @str: param
- * Return: list_t
+ *	_strlen - get length of string
+ * @s: string
+ * Return: num
  */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; ++s)
+	{
+		++i;
+	}
+	return (i);
+}
+
+
+/**
+ * add_node - add a node at the beginning of a linked list
+ * @head: double pointer to
+ * @str: string to add the struct
+ * Return: pointer to the newly created node
+ * Description: note that we could have used  single pointer
+ * if the linked list is empty
+ */
+
+
 list_t *add_node(list_t **head, const char *str)
 {
-	(void)head;
-	(void)str;
-	return (NULL);
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->len = _strlen(str);
+	new_node->str = strdup(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
