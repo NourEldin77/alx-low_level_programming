3b82212bff9f66b6d2d38104eecae0e43aa7c9d5
NourEldin77 <noureldean.2003@gmail.com>
2024-03-03 03:46:22 +0200

    Task: free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 
 /**
- * free_list - dosomething
- * @head: param
+ * free_list - free all mem that allocated for the linked list
+ * @head: pointer to head of the list
  * Return: void
  */
 void free_list(list_t *head)
 {
-	(void)head;
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
 }
