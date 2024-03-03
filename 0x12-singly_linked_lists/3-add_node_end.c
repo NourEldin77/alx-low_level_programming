ba61a822b334c6bb194050412e4c4fd7a7ecb0f5
NourEldin77 <noureldean.2003@gmail.com>
2024-03-03 03:26:10 +0200

    Task: add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,14 +1,33 @@
 #include "lists.h"
 
 /**
- * add_node_end - dosomething
- * @head: param
- * @str:param
- * Return: pointer list_t
+ * add_node_end - append node to the linked list
+ * @head: double pointer to the list
+ * @str:str to be copied to the new node
+ * Return: pointer to new created node
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	(void)head;
-	(void)str;
-	return (NULL);
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+	list_t *last_node = *head;
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->len = strlen(str);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+		return (NULL);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	while (last_node->next != NULL)
+	{
+		last_node = last_node->next;
+	}
+	last_node->next = new_node;
+	return (new_node);
 }
