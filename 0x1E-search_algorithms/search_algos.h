2059094c33de03a0fca714017be0b03939f07451
NourEldin77 <noureldean.2003@gmail.com>
2024-05-22 01:51:54 +0300

    init: header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef __HEAD__
+#define __HEAD__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif
